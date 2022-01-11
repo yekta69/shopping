9a0cdde725e42ccd513ea9af0efaeae3b2679950
hp <yekta_s69@yahoo.com>
2022-01-11 21:59:51 +0330

    fixed navbar

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -2,58 +2,40 @@
 import  'bootstrap/dist/css/bootstrap.rtl.min.css'
 import '../navbar/style.css'
 export const Navbar =() =>{
-return <div><nav className="navbar navbar-expand-lg navbar-light bg-light" >
+return <div>
     <div className='Logobar'>
         <ul>
-            <img className='logo' src={require("../navbar/index.jpg")}/>
-            <img className='Shopping' src={require("../navbar/shopping-cart.png")}/>
+            <img className='logo' src={require("../navbar/index.jpg")} alt='لوگو'/>
+              <div>
+               <button type="button" class="btn btn-outline-danger">ورود/ثبتنام</button>
+               <img className='Shopping' src={require("../navbar/shopping-cart.png")} alt ='سبدخرید'/>
+          </div>
         </ul>
       </div>
-  </nav> 
+ 
 
 <div className='baner'>
-       <img src={require('../navbar/images.jpg')} />
+       <img src={require('../navbar/onlineShop.jpg')}  alt='بنر'/>
 </div>
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
 <div className="container-fluid">
-  <a className="navbar-brand" href="#">Navbar</a>
-  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <a className="nav-link active" aria-current="page" href="http://www.google.com">خانه</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
-      </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown
-        </a>
-        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <li><a className="dropdown-item" href="#">Action</a></li>
-          <li><a className="dropdown-item" href="#">Another action</a></li>
-          <li><hr className="dropdown-divider"/></li>
-          <li><a className="dropdown-item" href="#">Something else here</a></li>
-        </ul>
+        <a className="nav-link active" aria-current="page" href="http://www.google.com">درباره ی ما</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a className="nav-link active" aria-current="page" href="http://www.google.com">ارتباط با ما</a>
       </li>
-    </ul>
+      </ul>
    </div>
 </div>
 </nav>
-<div>
-    <div>
-        <div>
-            <div>
-                
-            </div>
-        </div>
-    </div>
-</div>
 </div>
 }
