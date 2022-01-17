eb68434a327e86adda7d7928b89ab864155ba0ae
Zarvand <Zarvand@DESKTOP-JRS6OPQ>
2022-01-17 03:17:26 -0800

    fix

---
+++ src/component/productCard/index.js
@@ -0,0 +1,8 @@
+
+export const ProductCard =() =>{
+    return <div className="productCard">
+        <img src={require("")}/>
+        <img src={require("")}/>
+        <img src={require("")}/>
+        </div>
+ }
