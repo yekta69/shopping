bb99a37f9ac94c3214a3cfd3efd95df889c72d10
Zarvand <Zarvand@DESKTOP-JRS6OPQ>
2022-01-18 02:25:44 -0800

    listCard

--- src/component/productCard/index.js
+++ src/component/productCard/index.js
@@ -1,8 +1,26 @@
 
+import '../productCard/style.css'
 export const ProductCard =() =>{
     return <div className="productCard">
-        <img src={require("")}/>
-        <img src={require("")}/>
-        <img src={require("")}/>
+        <div className='mainCard'>
+        <div className='card'>
+            <h5 className='productname'> برنج اعلا</h5>
+            <img src={require("../productCard/images/product1.jpg")}/>
+            <h5 className='Price'></h5>
+            <button type="button" class="btn btn-danger"><img className='buy' src={require("../productCard/images/shopping-cart-Pre.png")}/></button>
         </div>
- }
+        <div className='card'>
+            <h5 className='productname'> برنج اعلا</h5>
+            <img src={require("../productCard/images/product2.png")}/>
+            <h5 className='Price'></h5>
+            <button type="button" class="btn btn-danger"><img className='buy' src={require("../productCard/images/shopping-cart-Pre.png")}/></button>
+        </div>
+        <div className='card'>
+            <h5 className='productname'> برنج اعلا</h5>
+            <img src={require("../productCard/images/product3.jpg")}/>
+            <h5 className='Price'></h5>
+            <button type="button" class="btn btn-danger"><img className='buy' src={require("../productCard/images/shopping-cart-Pre.png")}/></button>
+        </div>
+        </div>
+        </div>
+         }
