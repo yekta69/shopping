9051ab700baaa6714b4f503a7ea770d365a2094c
Zarvand <Zarvand@DESKTOP-JRS6OPQ>
2022-01-19 04:33:47 -0800

    set photo

---
+++ src/component/FixedPhoto/index.jsx
@@ -0,0 +1,7 @@
+
+import '../FixedPhoto/style.css'
+export const FixcedPhoto =() =>{
+    return <div className="FixcedPhoto">
+    <img src={require("../FixedPhoto/image/shopp.jpg")}/>
+    </div>
+}
