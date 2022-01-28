e62b1f01da6ae0ac260994fa84536f477a6c18bb
Meisam <meysam.valizadeh@utechia.com>
2022-01-28 21:10:46 +0330

    fix byg

--- src/component/specialOffer/index.js
+++ src/component/specialOffer/index.js
@@ -1,22 +1,33 @@
-
-import '../specialOffer/style.css'
-export const SpecialOffer =(props,{data}) =>{
-
-  console.log("ttttt",props)
-if(!data){
-  return<div>...loading</div>
-}
-
-return <div className="specialoffer">
-    <div className='specialofferData'>
-    <img src = {require("../specialOffer/image/spesial.jpg")} alt="پیشنهاد ویژه"/>
-    {/* <img src ={data.image} alt="پیشنهاد ویژه"/> */}
-       <div className='information'>
-      <h2>{data.firstname}</h2>
-     <h4>{data.lastName}</h4>
-      <h6>هر {data.nationalcode} : {data.password} تومان</h6>
-      <button type="button" className="btn btn-danger">حالا بخرید</button>
-      </div>  
+import { useEffect, useState } from "react";
+import "../specialOffer/style.css";
+export const SpecialOffer = (props) => {
+  const [data, setData] = useState(props.data);
+  useEffect(() => {
+    setData(props.data);
+  }, [props.data]);
+  console.log("ttttt", data);
+  if (!data) {
+    return <div>...loading</div>;
+  }
+  return (
+    <div className="specialoffer">
+      <div className="specialofferData">
+        <img
+          src={require("../specialOffer/image/spesial.jpg")}
+          alt="پیشنهاد ویژه"
+        />
+        {/* <img src ={data.image} alt="پیشنهاد ویژه"/> */}
+        <div className="information">
+          <h2>{data.firstname}</h2>
+          <h4>{data.lastName}</h4>
+          <h6>
+            هر {data.nationalcode} : {data.password} تومان
+          </h6>
+          <button type="button" className="btn btn-danger">
+            حالا بخرید
+          </button>
+        </div>
+      </div>
     </div>
-</div>
-}
+  );
+};
