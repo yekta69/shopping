ed96386adcb9d97a984f7f4814c52cf0c7b68e90
hp <yekta_s69@yahoo.com>
2022-05-17 22:54:42 +0430

    UpdateSendCode

--- src/component/LogIn/components/register.jsx
+++ src/component/LogIn/components/register.jsx
@@ -11,15 +11,5 @@ export const Register =() =>{
 
 return
 
-<form onSubmit={submitHandler}>
-    <h6> | ثبت نام</h6>
-    <p>لطفا  اطلاعات خود را وارد نمایید:</p>
-    <input value ={state.data.mobileNumber || ""}  name='mobileNumber' onChange={changeHandler}></input>
-    <small className='form-text text-danger'>
-    {state.errors.mobileNumber}
-    </small>
-    <button className="LoginButton" type='submit'>ورود</button>
-    <p className='note' > تمام حقوق این وب سایت متعلق به فروشگاه آنلاین کالانید است.</p>
- 
-</form>
+
 }
